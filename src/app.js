51f1c1a159240132a0c1469a7ba078ae67249b7e
sandeepdev99 <sandeep99.dev@gmail.com>
2025-12-27 16:01:20 +0530

    change d cors policy

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://backend-stm.onrender.com",
     Credential: true}
   ))
 
