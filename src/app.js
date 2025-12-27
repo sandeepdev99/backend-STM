989cf22ccdabb0812173be8ef439f6b5a09faf4f
sandeepdev99 <sandeep99.dev@gmail.com>
2025-12-27 16:04:03 +0530

    true cors policy

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 
 const app = express();
 app.use(cors({
-    origin: "https://backend-stm.onrender.com",
+    origin: "https://stmfrontend.vercel.app",
     Credential: true}
   ))
 
